a11096ffba61f1feba1b74178613a0ca919ba5e1
Mahesh29501 <mc8662230@gmail.com>
2023-01-06 21:55:37 +0530

    Resume download fix

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
-import Resume from '../assets/Mahesh_Chavan_Resume.pdf';
+import Resume from '../assets/MaheshChavan.pdf';
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
